c4d055b419d67a55110975a6ec3e09197c9961ad
tuepon <t.uepon@gmail.com>
2023-06-03 14:10:16 +0900

    add validations to model

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,3 +1,5 @@
 class Word < ApplicationRecord
-
+  validates :original, presence: true, length: { minimum: 1, maximum: 100 }
+  validates :translation, presence: true, length: { minimum: 1, maximum: 200 }
+  validates :description, presence: true, length: { maximum: 300 }
 end

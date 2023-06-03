e2288be15220aba7628f821db36a318f376e60de
tuepon <t.uepon@gmail.com>
2023-06-03 15:33:13 +0900

    update routes

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_02_121607) do
   create_table "words", force: :cascade do |t|
     t.string "original"
     t.string "translation"
-    t.text "description"
+    t.text "description", null: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

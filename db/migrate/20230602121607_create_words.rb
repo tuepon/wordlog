535b57c09307a5a9c96e9f3d334a2dd0a9b6b226
tuepon <t.uepon@gmail.com>
2023-06-03 12:26:36 +0900

    update schema file

--- db/migrate/20230602121607_create_words.rb
+++ db/migrate/20230602121607_create_words.rb
@@ -1,7 +1,7 @@
 class CreateWords < ActiveRecord::Migration[7.0]
   def change
     create_table :words do |t|
-      t.string :word
+      t.string :original
       t.string :translation
       t.text :description
       t.timestamps

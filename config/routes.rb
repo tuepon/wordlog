cf981fc99daa59bc20a68f6b14207e409d88a4a2
tuepon <t.uepon@gmail.com>
2023-07-20 23:53:36 +0900

    Add User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :words, only: [:show, :index, :new, :create, :edit, :update, :destroy]

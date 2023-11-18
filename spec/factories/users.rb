90d3542cd1bdf2569d7be96fc89d5940f088d6dc
tuepon <t.uepon@gmail.com>
2023-11-19 08:19:06 +0900

    install rspec and test user model

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :user do
-    
+    username { 'testuser' }
+    sequence :email do |n|
+      "test#{n}@example.com"
+    end
+    password { '123123' }
+    password_confirmation { '123123' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  let(:username) { 'testuser' }
+  let(:email) { 'test@example.com' }
+
+  describe '.first' do
+    before do
+      create(:user, username: username, email: email)
+    end
+
+    subject { described_class.first }
+
+    it 'Return user that has been created' do
+      expect(subject.username).to eq('testuser')
+      expect(subject.email).to eq('test@example.com')
+    end
+  end
 end

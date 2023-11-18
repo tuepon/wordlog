ef3aa9c06e1bcbff6c097103c3f523f349752db7
tuepon <t.uepon@gmail.com>
2023-11-18 23:05:28 +0900

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :words
+  has_one_attached :profile_pic
+
 end

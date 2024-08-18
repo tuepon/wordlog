63aa9544eaa5b9bcac31d9ce59e615be0cdfeee6
tuepon <t.uepon@gmail.com>
2024-08-18 18:46:21 +0900

    Update bin/render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,3 +5,5 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+# bundle exec rake db:migrate
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset

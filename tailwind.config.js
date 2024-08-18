fb91cef161c9a74c1ddb8036ab46e56220338e91
tuepon <t.uepon@gmail.com>
2024-08-18 14:26:36 +0900

    Update tailwindcss2 to 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,10 @@ module.exports = {
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
+    './app/javascript/**/*.js',
+    './config/initializers/simple_form_tailwind.rb',
   ],
-  plugins: [require("daisyui")]
+  plugins: [
+    require('daisyui'),
+  ],
 }

a64997a1d115c9e2fe476a8b2d2eb239594e4fe5
tuepon <t.uepon@gmail.com>
2024-08-20 21:09:41 +0900

    Restore migration

--- db/migrate/20231117141230_add_user_id_to_words.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToWords < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :words, :user, foreign_key: true
-  end
-end
--- db/migrate/20240819131652_rename_original_column_to_words.rb
+++ db/migrate/20240819131652_rename_original_column_to_words.rb
@@ -1,5 +1,5 @@
 class RenameOriginalColumnToWords < ActiveRecord::Migration[7.0]
   def change
-    rename_column :words, :original, :title
+    rename_column :words, :original, :title 
   end
 end

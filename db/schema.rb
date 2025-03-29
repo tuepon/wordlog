d68d733f9e8800a6bfb993a3bab46330c2aa719c
Tomonori Ueno <t.uepon@gmail.com>
2025-03-30 08:23:23 +0900

    Modify schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_08_04_000529) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -45,7 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_04_000529) do
   create_table "posts", force: :cascade do |t|
     t.string "caption"
     t.text "body"
-    t.bigint "user_id", null: false
+    t.integer "user_id", null: false
     t.boolean "allow_comments"
     t.boolean "show_likes_count"
     t.datetime "created_at", null: false
@@ -71,7 +68,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_04_000529) do
     t.string "title"
     t.string "translation"
     t.text "description"
-    t.bigint "user_id", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_words_on_user_id"

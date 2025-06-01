9329791fe8787beabc527a4546861b8ca9586215
Tomonori Ueno <t.uepon@gmail.com>
2025-06-01 09:01:26 +0900

    Update contact.rb

--- db/migrate/20250529085935_create_contacts.rb
+++ db/migrate/20250529085935_create_contacts.rb
@@ -1,8 +1,7 @@
 class CreateContacts < ActiveRecord::Migration[7.0]
   def change
     create_table :contacts do |t|
-      t.string :name
-
+      t.string :name, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -43,10 +43,10 @@ ActiveRecord::Schema[7.0].define(version: 2025_05_31_230624) do
   end
 
   create_table "contacts", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "email"
+    t.string "email", null: false
     t.string "telephone"
   end
 

ba794067b6b6ab2fcabc5306c8917ea9ebaf9588
Tomonori Ueno <t.uepon@gmail.com>
2025-06-03 11:00:55 +0900

    Modify contact_mailer.rb

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,9 @@
 class ContactMailer < ApplicationMailer
   def mail_to_user(contact_id)
     @contact = Contact.find(contact_id)
-    mail(to: @contact.email, subject: 'お問い合わせありがとうございます')
+    mail(
+      to: ENV.fetch('CONTACT_TEST_RECIPIENT', @contact.email), # @contact.email
+      subject: 'お問い合わせありがとうございます'
+    )
   end
 end

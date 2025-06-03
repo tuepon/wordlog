71f3283dd860b9cba10d0a9c14a9b912ea36327e
Tomonori Ueno <t.uepon@gmail.com>
2025-06-03 13:46:49 +0900

    Add logger to application.rb

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,7 @@ class ContactMailer < ApplicationMailer
   def mail_to_user(contact_id)
     @contact = Contact.find(contact_id)
     mail(
-      to: ENV.fetch('CONTACT_TEST_RECIPIENT', @contact.email), # @contact.email
+      to: ENV.fetch('CONTACT_TEST_RECIPIENT', @contact.email),
       subject: 'お問い合わせありがとうございます'
     )
   end

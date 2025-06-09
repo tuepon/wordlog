92ba0ce08dc308f032180886e0189598d0724233
Tomonori Ueno <t.uepon@gmail.com>
2025-06-09 21:43:10 +0900

    Add user activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :confirmable
 
   validates :username, presence: true, length: { maximum: 50 }
 

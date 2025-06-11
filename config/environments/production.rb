9cabb41093630c44c0f5874858f5cc1c533ece65
Tomonori Ueno <t.uepon@gmail.com>
2025-06-11 20:28:51 +0900

    Add domain name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,4 +101,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # My domain setting 
+  config.host << 'www.webisle.net'
 end

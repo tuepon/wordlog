efa7ccbf3b6298bac14cca0fba7702a0f3b36ae7
Tomonori Ueno <t.uepon@gmail.com>
2025-09-16 22:28:15 +0900

    Apply tailwindcss

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,21 +1,8 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./app/views/**/*.html.erb",
     "./app/helpers/**/*.rb",
     "./app/javascript/**/*.js",
-    "./app/javascript/stylesheets/pagy.tailwind.css",
   ],
-  safelist: [
-    "flex",
-    "space-x-1",
-    "font-semibold",
-    "text-sm",
-    "text-gray-500",
-    "hover:underline",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  plugins: [require("daisyui")],
 };

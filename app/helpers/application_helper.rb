40ec1b0a79a287819bc6357c7520a314858072e5
Tomonori Ueno <t.uepon@gmail.com>
2026-01-10 08:55:12 +0900

    Fix line break defect

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,12 @@ module ApplicationHelper
   include Pagy::Frontend
 
   def linkify(text)
-    return '' if text.blank?
+    return "" if text.blank?
 
     Rinku.auto_link(
-      simple_format(h(text)),
+      h(text),
       :all,
-      'class="text-blue-600 underline hover:text-blue-800" target="_blank" rel="noopener"'
-    ).html_safe
+      'target="_blank" rel="noopener" class="text-blue-600 underline hover:text-blue-800"'
+    )
   end
 end

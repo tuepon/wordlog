b1131b38dc2f1ec615f34640e5f0c66332d5159a
Tomonori Ueno <t.uepon@gmail.com>
2026-01-09 20:40:18 +0900

    Implement rinku

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def linkify(text)
+    return '' if text.blank?
+
+    Rinku.auto_link(
+      simple_format(h(text)),
+      :all,
+      'class="text-blue-600 underline hover:text-blue-800" target="_blank" rel="noopener noreferrer"'
+    ).html_safe
+  end
 end
